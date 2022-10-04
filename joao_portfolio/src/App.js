5fa15caf153b93b49023765413cc6ff7d6a29596
silvajpedro <belojpedro.31@gmail.com>
2022-10-03 22:16:14 -0300

    first commit

---
+++ joao_portfolio/src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function App(){
+  return(
+    <>
+    <img src="https://www.boavista.rr.leg.br/imagens/emconstruo.jpg/image" alt="" />
+    </>
+  )
+}
